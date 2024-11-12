33842c11a2ce91250eaa90f82b4e20eb8b4098c7
zngue <zngfue@163.com>
2024-11-12 15:35:35 +0800

    1

---
+++ cmd/zng/mian.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"gitee.com/zngue_mic/zng_tool/app"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+func main() {
+	//name   = "user-service"
+	var rootCmd = &cobra.Command{
+		Use:     "zng",
+		Short:   "zng: An elegant toolkit for Go microservices.",
+		Long:    `zng: An elegant toolkit for Go microservices.`,
+		Version: "1.0.0",
+	}
+	rootCmd.AddCommand(
+		&cobra.Command{
+			Use:  "new",
+			Long: "创建一个项目",
+			Run: func(cmd *cobra.Command, args []string) {
+				if len(args) == 0 {
+					fmt.Println("请输入项目名称")
+				}
+				fmt.Println(fmt.Sprintf("开始创建项目：%s", args[0]))
+				var err = run(args[0])
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println("项目创建成功")
+			},
+		},
+	)
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Println(err)
+	}
+	return
+}
+func run(name string) (err error) {
+	var (
+		url    = "https://gitee.com/zngue_mic/zng_layout.git"
+		branch = "master"
+	)
+	//判断文件夹是否存在
+	_, err = os.Stat(name)
+	if err == nil {
+		fmt.Println(fmt.Sprintf("改项目已存在：%s", name))
+		return
+	}
+	repo := app.NewRepo(url, branch)
+	ctx := context.Background()
+	err = repo.RemoTo(
+		ctx,
+		name,
+		[]string{".git", ".github", "go.sum"},
+		[]string{
+			"github.com/zngue/zng_layout",
+			fmt.Sprintf("github.com/zngue_mic/%s", name),
+			"zng_layout",
+			name,
+		},
+	)
+	return
+}
---
+++ go.mod
@@ -0,0 +1,17 @@
+module gitee.com/zngue_mic/zng_tool
+
+go 1.23.2
+
+require (
+	github.com/fatih/color v1.18.0
+	github.com/spf13/cobra v1.8.1
+	golang.org/x/mod v0.8.0
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
