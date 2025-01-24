5a3ad3ef5bdabf01f9004417e92e720195b04c49
zngue <zngfue@163.com>
2025-01-24 16:48:03 +0800

    更新文件777

---
+++ abc-ac.go
@@ -0,0 +1,41 @@
+package service
+
+import (
+	empty "github.com/golang/protobuf/ptypes/empty"
+)
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zngue/zng_tool/api/test/v1"
+)
+
+type TestService struct {
+}
+
+func NewTestService() v1.TestGinHttpService {
+	return &TestService{}
+}
+func (s *TestService) Info(ctx *gin.Context, req *v1.GetTestRequest) (rs *v1.GetTestReply, err error) {
+
+	return
+}
+func (s *TestService) List(ctx *gin.Context, req *v1.GetListTestRequest) (rs *v1.GetTestReply, err error) {
+
+	return
+}
+func (s *TestService) Create(ctx *gin.Context, req *v1.GetTestRequest) (rs *v1.GetTestReply, err error) {
+
+	return
+}
+func (s *TestService) ListUser(ctx *gin.Context, req *v1.GetListTestRequest) (rs *v1.UserList, err error) {
+
+	return
+}
+func (s *TestService) ListUser2(ctx *gin.Context, req *v1.GetList2TestRequest) (rs *v1.UserList2Rely, err error) {
+
+	return
+}
+func (s *TestService) UpdateUser(ctx *gin.Context, req *v1.UpdateUserRequest) (rs *empty.Empty, err error) {
+
+	return
+}
